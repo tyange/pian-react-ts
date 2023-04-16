7023dc632fc189dcdfeddf27119fa2bbb86d3614
tyange <usun16@gmail.com>
2023-04-16 15:18:54 +0900

    burger list rendering with Grid

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import BurgerAPI from "../api/burger/BurgerAPI";
 
 import Layout from "../components/layout/Layout";
 import { CircularProgress, Container } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 import BurgerItem from "../components/burger-item/BurgerItem";
 
 const MainPage = () => {
@@ -18,21 +19,22 @@ const MainPage = () => {
 
   return (
     <Layout>
-      <Container>
+      <Grid container rowSpacing={3}>
         {isLoading && <CircularProgress />}
         {data &&
           !isLoading &&
           !error &&
           data.map((burger, index) => (
-            <BurgerItem
-              key={index}
-              userId={burger.userId}
-              name={burger.name}
-              brand={burger.brand}
-              description={burger.description}
-            />
+            <Grid xs={2} sm={4} md={4} key={index}>
+              <BurgerItem
+                userId={burger.userId}
+                name={burger.name}
+                brand={burger.brand}
+                description={burger.description}
+              />
+            </Grid>
           ))}
-      </Container>
+      </Grid>
     </Layout>
   );
 };
